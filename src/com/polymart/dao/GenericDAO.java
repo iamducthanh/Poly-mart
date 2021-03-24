fe412b9f95437f3cab91e2eef7e92db4915c063f
VDB <namlun96@gmail.com>
2021-03-24 11:17:43 +0700

    add class kết nối CSDL + hàm insert update

--- src/com/polymart/dao/GenericDAO.java
+++ src/com/polymart/dao/GenericDAO.java
@@ -1,5 +1,13 @@
 package com.polymart.dao;
 
-public interface GenericDAO {
+import java.util.List;
 
+import com.polymart.mapper.RowMapper;
+
+public interface GenericDAO <T> {
+	<T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters);
+	void update (String sql, Object... parameters);
+	
+	//insert câu lệnh sql với tham số truyền vào parameters, trả về mã id
+	Long insert (String sql, Object... parameters);
 }

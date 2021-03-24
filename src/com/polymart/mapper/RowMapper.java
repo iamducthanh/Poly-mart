65173d641f32df995cacca5db9d8f1666ac01540
iamducthanh <ducthanh260801@gmail.com>
2021-03-24 18:51:51 +0700

    update form

--- src/com/polymart/mapper/RowMapper.java
+++
@@ -1,7 +0,0 @@
-package com.polymart.mapper;
-
-import java.sql.ResultSet;
-
-public interface RowMapper<T> {
-	T mapRow(ResultSet rs);
-}
--- src/com/polymart/model/AbstractModel.java
+++ src/com/polymart/model/AbstractModel.java
@@ -1,15 +1,5 @@
 package com.polymart.model;
 
 public class AbstractModel {
-	private Long id;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	
 }

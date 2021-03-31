99a1d32ebe2e90b6748a43dbe61febced3337d71
VDB <namlun96@gmail.com>
2021-03-31 21:06:08 +0700

    add fn tìm kiếm theo tên hoặc id nhân viên (filterByIdAndName)

--- src/com/polymart/dao/INhanVienDAO.java
+++ src/com/polymart/dao/INhanVienDAO.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 import com.polymart.model.NhanVienModel;
 
-public interface INhanVienDAO extends GenericDAO<NhanVienModel> {
+public interface INhanVienDAO {
 	List<NhanVienModel> findAll();
 
 	Integer save(NhanVienModel nhanVienModel);
-	
+
 	void update(NhanVienModel nhanVienModel);
-	
+
 	void delete(Integer id);
-	
+
 	NhanVienModel findOne(Integer id);
+
+	List<NhanVienModel> filterByIdAndName(String idOrName);
 }

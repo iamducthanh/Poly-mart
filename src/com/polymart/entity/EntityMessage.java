463937e2ed3c535c8d0820d2f92e5ac5205a318a
NgocPJa <quangngoc201197@gmail.com>
2021-03-31 22:58:01 +0700

    edit form change password

--- src/com/polymart/entity/EntityMessage.java
+++ src/com/polymart/entity/EntityMessage.java
@@ -6,16 +6,21 @@ import javax.swing.JOptionPane;
 
 public class EntityMessage {
 
-    // tiêu đề của phần mềm
-    public static String TITLE = "Thông báo";
+	// tiêu đề của phần mềm
+	public static String TITLE = "Thông báo";
 
-    public static void show(Component component, String text) {
-        JOptionPane.showMessageDialog(component, text, TITLE, JOptionPane.INFORMATION_MESSAGE);
-    }
+	public static void show(Component component, String text) {
+		JOptionPane.showMessageDialog(component, text, TITLE, JOptionPane.INFORMATION_MESSAGE);
+	}
 
-    // hàm hiển thị xác nhận của người dùng
-    public static boolean confirm(Component component, String text) {
-        return JOptionPane.showConfirmDialog(component, text, TITLE,
-                JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
-    }
+	// hàm hiển thị xác nhận của người dùng
+	public static boolean confirm(Component component, String text) {
+		return JOptionPane.showConfirmDialog(component, text, TITLE,
+				JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
+	}
+
+	// hàm hiển cho phép người dùng nhập dữ liệu vào
+	public static String porm(Component component, String text) {
+		return JOptionPane.showInputDialog(component, text, TITLE, JOptionPane.INFORMATION_MESSAGE);
+	}
 }

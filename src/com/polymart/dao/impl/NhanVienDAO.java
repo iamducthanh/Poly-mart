39253b0751d40aa38e8d85428869baf1f161c435
DB <DB@DB>
2021-04-02 03:17:48 +0700

    add fn filter successful

--- src/com/polymart/dao/impl/NhanVienDAO.java
+++ src/com/polymart/dao/impl/NhanVienDAO.java
@@ -10,22 +10,22 @@ import com.polymart.service.impl.NhanVienService;
 
 public class NhanVienDAO extends AbstractDAO<NhanVienModel> implements INhanVienDAO {
 
-    @Override
-    public List<NhanVienModel> findAll() {
-        String sql = "SELECT*FROM NHANVIEN";
-        return query(sql, new NhanVienMapper());
-    }
+	@Override
+	public List<NhanVienModel> findAll() {
+		String sql = "SELECT*FROM NHANVIEN";
+		return query(sql, new NhanVienMapper());
+	}
 
-    @Override
-    public Integer save(NhanVienModel nhanVienModel) {
-        StringBuilder sql = new StringBuilder("INSERT INTO NHANVIEN (HOTEN, ");
-        sql.append("NGAYSINH, GIOITINH, DIACHI, SODIENTHOAI, EMAIL, CHUCVU, ");
-        sql.append("MUCLUONG, ANHDAIDIEN, MATKHAU) ");
-        sql.append("VALUES (?,?,?,?,?,?,?,?,?,?)");
-        return insert(sql.toString(), nhanVienModel.getHoTen(), nhanVienModel.getNgaySinh(), nhanVienModel.isGioiTinh(),
-                nhanVienModel.getDiaChi(), nhanVienModel.getSdt(), nhanVienModel.getEmail(), nhanVienModel.getChucVu(),
-                nhanVienModel.getLuong(), nhanVienModel.getAnhDaiDien(), nhanVienModel.getMatKhau());
-    }
+	@Override
+	public Integer save(NhanVienModel nhanVienModel) {
+		StringBuilder sql = new StringBuilder("INSERT INTO NHANVIEN (HOTEN, ");
+		sql.append("NGAYSINH, GIOITINH, DIACHI, SODIENTHOAI, EMAIL, CHUCVU, ");
+		sql.append("MUCLUONG, ANHDAIDIEN, MATKHAU) ");
+		sql.append("VALUES (?,?,?,?,?,?,?,?,?,?)");
+		return insert(sql.toString(), nhanVienModel.getHoTen(), nhanVienModel.getNgaySinh(), nhanVienModel.isGioiTinh(),
+				nhanVienModel.getDiaChi(), nhanVienModel.getSdt(), nhanVienModel.getEmail(), nhanVienModel.getChucVu(),
+				nhanVienModel.getLuong(), nhanVienModel.getAnhDaiDien(), nhanVienModel.getMatKhau());
+	}
 
 	@Override
 	public void update(NhanVienModel nhanVienModel) {
@@ -39,31 +39,28 @@ public class NhanVienDAO extends AbstractDAO<NhanVienModel> implements INhanVien
 				nhanVienModel.getId());
 	}
 
-    @Override
-    public void delete(Integer id) {
-        String sql = "DELETE NHANVIEN WHERE ID = ?";
-        update(sql, id);
-    }
+	@Override
+	public void delete(Integer id) {
+		String sql = "DELETE NHANVIEN WHERE ID = ?";
+		update(sql, id);
+	}
 
-    @Override
-    public NhanVienModel findOne(Integer id) {
-        String sql = "SELECT*FROM NHANVIEN WHERE ID = ?";
-        List<NhanVienModel> listNhanVien = query(sql, new NhanVienMapper(), id);
-        return listNhanVien.isEmpty() ? null : listNhanVien.get(0);
-    }
+	@Override
+	public NhanVienModel findOne(Integer id) {
+		String sql = "SELECT*FROM NHANVIEN WHERE ID = ?";
+		List<NhanVienModel> list = query(sql, new NhanVienMapper(), id);
+		return list.isEmpty() ? null : list.get(0);
+	}
 
 	@Override
 	public List<NhanVienModel> filterByIdAndName(String idOrName) {
-//		String sql = "SELECT*FROM NHANVIEN WHERE ID LIKE ? OR HOTEN LIKE N?";
-//		return query(sql, new NhanVienMapper(), "%" + idOrName + "%", "%" + idOrName + "%'");
-
-		String sql = "SELECT*FROM NHANVIEN WHERE HOTEN LIKE N?";
-		return query(sql, new NhanVienMapper(), "%" + idOrName + "%");
+		String sql = "SELECT*FROM NHANVIEN WHERE ID LIKE ? OR HOTEN LIKE ?";
+		return query(sql, new NhanVienMapper(), "%" + idOrName + "%", "%" + idOrName + "%");
 	}
 
 	public static void main(String[] args) {
 		INhanVienService nv = new NhanVienService();
-		List<NhanVienModel> list = nv.filterByIdAndName("Bùi");
+		List<NhanVienModel> list = nv.filterByIdAndName("6");
 		if (list.isEmpty()) {
 			System.out.println("empty");
 		} else {

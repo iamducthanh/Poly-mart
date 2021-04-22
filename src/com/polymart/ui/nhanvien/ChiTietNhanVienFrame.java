7fb5975133892a3d7c1af53cbafd14e2cbe16919
quang <quangngoc201197@gmail.com>
2021-04-22 20:20:11 +0700

    sửa lỗi tìm kiếm mã hàng hoá cũ

--- src/com/polymart/ui/nhanvien/ChiTietNhanVienFrame.java
+++ src/com/polymart/ui/nhanvien/ChiTietNhanVienFrame.java
@@ -331,7 +331,6 @@ public class ChiTietNhanVienFrame extends JFrame {
 		txtNgaySinh.setCalendar(null);
 		buttonGroup.clearSelection();
 		comboBox.setSelectedIndex(0);
-		txtMatKhau.setText(null);
 		txtMucLuong.setText(null);
 		txtSDT.setText(null);
 		txtEmail.setText(null);
